6cdd998215a8c4e2c53ad31fbb98893eb3e5b220
pmitko <pmitko@pixelfederation.com>
2020-02-05 17:36:32 +0100

    fixed repo name

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="pip git dependency",
+    name="pip git dependency test",
     packages=find_packages(),
     include_package_data=True,
     install_requires=[
