26b30f037564c47db856e86a8ae10bca91fa770c
pmitko <pmitko@pixelfederation.com>
2020-02-05 17:32:23 +0100

    fixed dependency name

--- setup.py
+++ setup.py
@@ -5,6 +5,6 @@ setup(
     packages=find_packages(),
     include_package_data=True,
     install_requires=[
-        "db@git+ssh://git@github.com/pmitko/pip_git_source.git@master#egg=db"
+        "db@git+ssh://git@github.com/pmitko/pip_git_dependency.git@master#egg=db"
     ]
 )
